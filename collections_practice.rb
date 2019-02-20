d2a9fe9ccfb37cb6dfba76c7b42569ec5559146c
Chhaian Pin <chhaianpin@hotmail.com>
2019-02-20 20:16:13 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,53 @@
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |x, y|
+    x.length <=> y.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |name|
+    name[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select do |string|
+    string.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  sum = 0
+  array.each do |num|
+    sum += num
+  end
+  sum
+end
+
+def add_s(array)
+  array.each do |plus_s|
+    if plus_s == array[1]
+      next
+    end
+    plus_s += "s"
+  end
+end
+
 
